238ff485f53e7063f17c7de153011ec5909afd7e
YEO Yédjandé <slateryeo@gmail.com>
2025-09-02 13:00:55 +0000

    Ajout de bases64 pour les images encore

--- server/db.ts
+++ server/db.ts
@@ -4,7 +4,7 @@ import * as schema from "@shared/schema";
 
 if (!process.env.DATABASE_URL) {
   throw new Error(
-    "DATABASE_URL must be set. Did you forget to provision a database?",
+    "DATABASE_URL must be set. Did you forget to provision a database???",
   );
 }
 
